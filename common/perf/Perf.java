ae641c4394ca5c963124ba7033259bdf8c595c87
JCY <jcy940818@naver.com>
2022-03-23 16:43:44 +0900

    index 필드 추가

--- common/perf/Perf.java
+++ common/perf/Perf.java
@@ -1,6 +1,10 @@
 package common.perf;
 
 public abstract class Perf {
+	private int index;
+	 
+	public int getIndex() { return index; }
+	public void setIndex(int index) { this.index = index; }
 	public abstract String getDisplayName();
 	public abstract String getCounter();
 	public abstract int getInterval();
