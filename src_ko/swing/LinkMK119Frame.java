ccf848ebe15440f7e2bdcbdea2f892ba30c05be7
정창용 <jcy940818@naver.com>
2022-04-19 23:36:05 +0900

    MK119 Link 프레임 UI 작업 백업

--- src_ko/swing/LinkMK119Frame.java
+++ src_ko/swing/LinkMK119Frame.java
@@ -18,6 +18,8 @@ import javax.swing.border.LineBorder;
 import src_ko.info.ONION_Info;
 import src_ko.main.MoonInspector;
 import src_ko.util.Util;
+import javax.swing.JSeparator;
+import java.awt.SystemColor;
 
 public class LinkMK119Frame extends JFrame{
 	
@@ -34,7 +36,7 @@ public class LinkMK119Frame extends JFrame{
 		setIconImage(new Util().getIconResource().getImage());
 		setResizable(false);
 		
-		setSize(480, 255);
+		setSize(545, 673);
 		setBackground(Color.WHITE);
 		getContentPane().setLayout(new BorderLayout(0, 0));
 
@@ -49,7 +51,7 @@ public class LinkMK119Frame extends JFrame{
 //		mk119Link.setForeground(Color.BLACK);
 		mk119Link.setBackground(Color.WHITE);
 		mk119Link.setIcon(new Util().getSubLogoResource());
-		mk119Link.setBounds(12, 10, 450, 50);
+		mk119Link.setBounds(12, 10, 515, 50);
 		mk119Link.setHorizontalAlignment(SwingConstants.LEFT);
 		mk119Link.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 21));
 		actualPanel.add(mk119Link);
@@ -61,7 +63,7 @@ public class LinkMK119Frame extends JFrame{
 		linkMK119Protocol_Button.setBackground(Color.WHITE);
 		linkMK119Protocol_Button.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 16));
 		linkMK119Protocol_Button.setFocusPainted(false);
-		linkMK119Protocol_Button.setBounds(60, 97, 365, 37);		
+		linkMK119Protocol_Button.setBounds(53, 316, 365, 37);		
 		linkMK119Protocol_Button.setEnabled(true);
 		linkMK119Protocol_Button.addActionListener(new ActionListener() {
 			@Override
@@ -81,7 +83,7 @@ public class LinkMK119Frame extends JFrame{
 		linkMK119PerfData_Button.setBackground(Color.WHITE);
 		linkMK119PerfData_Button.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 16));
 		linkMK119PerfData_Button.setFocusPainted(false);
-		linkMK119PerfData_Button.setBounds(60, 155, 365, 37);
+		linkMK119PerfData_Button.setBounds(53, 516, 365, 37);
 		linkMK119PerfData_Button.setEnabled(true);
 		linkMK119PerfData_Button.addActionListener(new ActionListener() {
 			@Override
@@ -91,6 +93,80 @@ public class LinkMK119Frame extends JFrame{
 			}
 		});
 		actualPanel.add(linkMK119PerfData_Button);
+		
+		JLabel mk119DB = new JLabel("1. MK119 Database Information");
+		mk119DB.setHorizontalAlignment(SwingConstants.LEFT);
+		mk119DB.setForeground(new Color(0, 128, 0));
+		mk119DB.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 20));
+		mk119DB.setBackground(Color.WHITE);
+		mk119DB.setBounds(27, 82, 500, 37);
+		actualPanel.add(mk119DB);
+		
+		JLabel mk119SqlServer = new JLabel("SQL Server : " + ONION_Info.getSimpleSqlServerInfo());
+		mk119SqlServer.setHorizontalAlignment(SwingConstants.LEFT);
+		mk119SqlServer.setForeground(Color.BLACK);
+		mk119SqlServer.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 17));
+		mk119SqlServer.setBackground(Color.WHITE);
+		mk119SqlServer.setBounds(53, 117, 474, 37);
+		actualPanel.add(mk119SqlServer);
+		
+		JLabel mk119DbName = new JLabel("DB Name : " + ONION_Info.getDataBaseName());
+		mk119DbName.setHorizontalAlignment(SwingConstants.LEFT);
+		mk119DbName.setForeground(Color.BLACK);
+		mk119DbName.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 17));
+		mk119DbName.setBackground(Color.WHITE);
+		mk119DbName.setBounds(53, 154, 474, 37);
+		actualPanel.add(mk119DbName);
+		
+		JLabel mk119ProtocolDataLink = new JLabel("2. MK119 Protocol Data Link");
+		mk119ProtocolDataLink.setHorizontalAlignment(SwingConstants.LEFT);
+		mk119ProtocolDataLink.setForeground(new Color(0, 128, 0));
+		mk119ProtocolDataLink.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 20));
+		mk119ProtocolDataLink.setBackground(Color.WHITE);
+		mk119ProtocolDataLink.setBounds(27, 223, 500, 37);
+		actualPanel.add(mk119ProtocolDataLink);
+		
+		JLabel mk119ProtocolVersion = new JLabel("Protocol : ÇÁ·ÎÅäÄÝ Á¤º¸ ¿¬µ¿ Àü");
+		mk119ProtocolVersion.setHorizontalAlignment(SwingConstants.LEFT);
+		mk119ProtocolVersion.setForeground(Color.BLACK);
+		mk119ProtocolVersion.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 17));
+		mk119ProtocolVersion.setBackground(Color.WHITE);
+		mk119ProtocolVersion.setBounds(53, 270, 474, 37);
+		actualPanel.add(mk119ProtocolVersion);
+		
+		JLabel mk119RestDataLink = new JLabel("3. MK119 REST API Data Link");
+		mk119RestDataLink.setHorizontalAlignment(SwingConstants.LEFT);
+		mk119RestDataLink.setForeground(new Color(0, 128, 0));
+		mk119RestDataLink.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 20));
+		mk119RestDataLink.setBackground(Color.WHITE);
+		mk119RestDataLink.setBounds(27, 397, 500, 37);
+		actualPanel.add(mk119RestDataLink);
+		
+		JLabel mk119RestStatus = new JLabel("Status : REST API ¿¬µ¿ Àü");
+		mk119RestStatus.setHorizontalAlignment(SwingConstants.LEFT);
+		mk119RestStatus.setForeground(Color.BLACK);
+		mk119RestStatus.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 17));
+		mk119RestStatus.setBackground(Color.WHITE);
+		mk119RestStatus.setBounds(53, 432, 474, 37);
+		actualPanel.add(mk119RestStatus);
+		
+		JLabel mk119RestAdminInfo = new JLabel("AdminConsole : REST API ¿¬µ¿ Àü");
+		mk119RestAdminInfo.setHorizontalAlignment(SwingConstants.LEFT);
+		mk119RestAdminInfo.setForeground(Color.BLACK);
+		mk119RestAdminInfo.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 17));
+		mk119RestAdminInfo.setBackground(Color.WHITE);
+		mk119RestAdminInfo.setBounds(53, 469, 474, 37);
+		actualPanel.add(mk119RestAdminInfo);
+		
+		JSeparator separator = new JSeparator();
+		separator.setForeground(new Color(160, 160, 160));
+		separator.setBounds(5, 210, 528, 19);
+		actualPanel.add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setForeground(SystemColor.controlShadow);
+		separator_1.setBounds(5, 380, 528, 19);
+		actualPanel.add(separator_1);
 		
 		if(linkProtocol && linkPerfData) {
 			mk119Link.setText("MK119 Data Link Completed");
@@ -107,5 +183,4 @@ public class LinkMK119Frame extends JFrame{
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
-	
 }
