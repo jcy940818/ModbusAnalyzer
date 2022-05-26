11b5e4467cc40a55a324bad4ce483a3e68b3207d
정창용 <jcy940818@naver.com>
2022-05-26 11:50:49 +0900

    ONION 멤버 추가

--- common/OnionMember.java
+++ common/OnionMember.java
@@ -94,6 +94,7 @@ public class OnionMember {
 		new OnionMember("PSÆÀ", "ÀÌÀç¼º", "Woody");
 		new OnionMember("PSÆÀ", "ÀÌÇüÂù", "Rick");
 		new OnionMember("PSÆÀ", "±è¿ø±â", "Dustin");
+		new OnionMember("PSÆÀ", "¼Ûµ¿Çö", "Danny");
 		
 		new OnionMember("½Ì°¡ÆúÆÀ", "Kelvin", "Kelvin");
 		new OnionMember("½Ì°¡ÆúÆÀ", "Jia Hui", "Jia");
@@ -102,7 +103,6 @@ public class OnionMember {
 		new OnionMember("½Ì°¡ÆúÆÀ", "SoonMeng", "SoonMeng");
 		new OnionMember("½Ì°¡ÆúÆÀ", "Tee", "Tee");
 		new OnionMember("½Ì°¡ÆúÆÀ", "Miffy", "Miffy");
-		
 	}
 	
 	
