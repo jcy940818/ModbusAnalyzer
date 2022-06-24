f5895740698ca866f0f59b41096ec5f758c7af56
정창용 <jcy940818@naver.com>
2022-06-24 14:14:54 +0900

    모드버스 포인트 정보 출력 양식 수정

--- common/modbus/ModbusWatchPoint.java
+++ common/modbus/ModbusWatchPoint.java
@@ -300,10 +300,10 @@ public class ModbusWatchPoint extends FmsPerfItem implements Comparable {
 			StringBuilder sb = new StringBuilder();
 			sb.append(String.format("%s%s%s\n", Util.colorGreen("Modbus Point Information"), Util.separator, Util.separator));
 			
-			sb.append(String.format("%s : %s", Util.colorBlue("1. 포인트 이름"), wp.getDisplayName()));
+			sb.append(String.format("%s : %s", Util.colorBlue("1. Point Name"), wp.getDisplayName()));
 			sb.append(Util.separator + Util.separator + "\n\n");
 			
-			sb.append(String.format("%s : %s", Util.colorBlue("2. 카운터(드라이버 코드)"), wp.getCounter()));
+			sb.append(String.format("%s : %s", Util.colorBlue("2. Counter"), wp.getCounter()));
 			sb.append(Util.separator + Util.separator + "\n");
 			
 			
@@ -313,49 +313,49 @@ public class ModbusWatchPoint extends FmsPerfItem implements Comparable {
 			
 			
 			sb.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
-			sb.append(String.format("%s : %s", Util.colorBlue("├&nbsp;&nbsp;기능코드"), wp.getFunctionCode()));
+			sb.append(String.format("%s : %s", Util.colorBlue("├&nbsp;&nbsp;Function Code"), wp.getFunctionCode()));
 			sb.append(Util.separator + Util.separator + "\n");
 			
 			sb.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
-			sb.append(String.format("%s : %s", Util.colorBlue("├&nbsp;&nbsp;모드버스 주소 ( DEC )"), wp.getModbusAddrString()));
+			sb.append(String.format("%s : %s", Util.colorBlue("├&nbsp;&nbsp;Modbus Address ( DEC )"), wp.getModbusAddrString()));
 			sb.append(Util.separator + Util.separator + "\n");
 			
 			sb.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
-			sb.append(String.format("%s : %d", Util.colorBlue("├&nbsp;&nbsp;레지스터 주소 ( DEC )"), wp.getRegisterAddr()));
+			sb.append(String.format("%s : %d", Util.colorBlue("├&nbsp;&nbsp;Register Address ( DEC )"), wp.getRegisterAddr()));
 			sb.append(Util.separator + Util.separator + "\n");
 			
 			sb.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
-			sb.append(String.format("%s : %s", Util.colorBlue("├&nbsp;&nbsp;레지스터 주소 ( HEX )"), wp.getRegisterAddrHexString()));
+			sb.append(String.format("%s : %s", Util.colorBlue("├&nbsp;&nbsp;Register Address ( HEX )"), wp.getRegisterAddrHexString()));
 			sb.append(Util.separator + Util.separator + "\n");
 			
 			
 			sb.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
-			sb.append(String.format("%s : %s", Util.colorBlue("└&nbsp;&nbsp;데이터 타입"), wp.getDataType()));
+			sb.append(String.format("%s : %s", Util.colorBlue("└&nbsp;&nbsp;Data Type"), wp.getDataType()));
 			sb.append(Util.separator + Util.separator + "\n\n");
 			
-			sb.append(String.format("%s : %s", Util.colorBlue("3. 측정 단위"), wp.getMeasure()));
+			sb.append(String.format("%s : %s", Util.colorBlue("3. Measure"), wp.getMeasure()));
 			sb.append(Util.separator + Util.separator + "\n\n");
 			
-			sb.append(String.format("%s : %s", Util.colorBlue("4. 보정식"), wp.getScaleFunction()));
+			sb.append(String.format("%s : %s", Util.colorBlue("4. Scale Formula"), wp.getScaleFunction()));
 			sb.append(Util.separator + Util.separator + "\n\n");
 			
 			String dataFormat = null;
 			switch(wp.getDataFormat()) {
 				case 1 : 
-					dataFormat = "1 ( DI : 이진 상태 )";
+					dataFormat = "1 ( Binary State )";
 					break;
 				case 2 : 
-					dataFormat = "2 ( Multiple : 다중 상태 )";
+					dataFormat = "2 ( Multiple State )";
 					break;
 				case 3 : 
-					dataFormat = "3 ( Analog : 아날로그 )";
+					dataFormat = "3 ( Analog )";
 					break;
 				default : 
-					dataFormat = "3 ( Analog : 아날로그 )";
+					dataFormat = "3 ( Analog )";
 					break;
 			}
 			
-			sb.append(String.format("%s : %s", Util.colorBlue("5. 데이터 형식"), dataFormat));
+			sb.append(String.format("%s : %s", Util.colorBlue("5. Data Format"), dataFormat));
 			sb.append(Util.separator + Util.separator + "\n");
 			
 			if(wp.getDataFormat() == 1) {
@@ -370,10 +370,10 @@ public class ModbusWatchPoint extends FmsPerfItem implements Comparable {
 				sb.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
 				sb.append(String.format("%s%s : %s&nbsp;&nbsp;%s&nbsp;&nbsp;%s : %s",
 						Util.colorBlue("├&nbsp;&nbsp;"),
-						Util.colorBlue("값"),
+						Util.colorBlue("Value"),
 						"0",
 						Util.colorRed("/"),
-						Util.colorBlue("내용"),
+						Util.colorBlue("Label"),
 						binLabel[0]));
 				sb.append(Util.separator + Util.separator + "\n");
 								
@@ -382,10 +382,10 @@ public class ModbusWatchPoint extends FmsPerfItem implements Comparable {
 				sb.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
 				sb.append(String.format("%s%s : %s&nbsp;&nbsp;%s&nbsp;&nbsp;%s : %s",
 						Util.colorBlue("└&nbsp;&nbsp;"),
-						Util.colorBlue("값"),
+						Util.colorBlue("Value"),
 						"1",
 						Util.colorRed("/"),
-						Util.colorBlue("내용"),
+						Util.colorBlue("Label"),
 						binLabel[1]));
 				sb.append(Util.separator + Util.separator + "\n");
 				
@@ -402,10 +402,10 @@ public class ModbusWatchPoint extends FmsPerfItem implements Comparable {
 					sb.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
 					sb.append(String.format("%s%s : %s&nbsp;&nbsp;%s&nbsp;&nbsp;%s : %s",
 							Util.colorBlue((i != (labels.length - 1)) ? "├&nbsp;&nbsp;" : "└&nbsp;&nbsp;"),
-							Util.colorBlue("값"),
+							Util.colorBlue("Value"),
 							labels[i].value,
 							Util.colorRed("/"),
-							Util.colorBlue("내용"),
+							Util.colorBlue("Label"),
 							labels[i].label));
 					sb.append(Util.separator + Util.separator + "\n");
 				}
@@ -470,13 +470,13 @@ public class ModbusWatchPoint extends FmsPerfItem implements Comparable {
 		StringBuilder sb = new StringBuilder();
 		sb.append("<font color='green'>Bit Structure</font>\n");
 		
-		sb.append(String.format(" <font color='blue'>포인트 이름</font> : %s%s%s\n",point.getDisplayName() ,Util.separator, Util.separator));
-		sb.append(String.format(" <font color='blue'>데이터 타입</font> : %s%s%s\n",dataType ,Util.separator, Util.separator));
-		sb.append(String.format(" <font color='blue'>보정식</font> : %s%s%s\n\n",point.getScaleFunction() ,Util.separator, Util.separator));
+		sb.append(String.format(" <font color='blue'>Point Name</font> : %s%s%s\n",point.getDisplayName() ,Util.separator, Util.separator));
+		sb.append(String.format(" <font color='blue'>Data Type</font> : %s%s%s\n",dataType ,Util.separator, Util.separator));
+		sb.append(String.format(" <font color='blue'>Scale Formula</font> : %s%s%s\n\n",point.getScaleFunction() ,Util.separator, Util.separator));
 		
-		sb.append(String.format(" <font color='blue'>모드버스 주소 (DEC)</font> : %s%s%s\n",point.getModbusAddrString() ,Util.separator, Util.separator));
-		sb.append(String.format(" <font color='blue'>레지스터 주소 (DEC)</font> : %d%s%s\n", point.getRegisterAddr()  ,Util.separator, Util.separator));
-		sb.append(String.format(" <font color='blue'>레지스터 주소 (HEX)</font> : %s%s%s\n\n",point.getRegisterAddrHexString() ,Util.separator, Util.separator));
+		sb.append(String.format(" <font color='blue'>Modbus Address ( DEC )</font> : %s%s%s\n",point.getModbusAddrString() ,Util.separator, Util.separator));
+		sb.append(String.format(" <font color='blue'>Register Address ( DEC )</font> : %d%s%s\n", point.getRegisterAddr()  ,Util.separator, Util.separator));
+		sb.append(String.format(" <font color='blue'>Register Address ( HEX )</font> : %s%s%s\n\n",point.getRegisterAddrHexString() ,Util.separator, Util.separator));
 		
 		sb.append(String.format(" <font color='blue'>Decimal</font> : %d%s%s\n",lastValue ,Util.separator, Util.separator));
 		sb.append(String.format(" <font color='blue'>Binary</font> : "));
