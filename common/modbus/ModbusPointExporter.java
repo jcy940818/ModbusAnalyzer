07f9488fda90722b5b50162197e4eadf7b2368e0
정창용 <jcy940818@naver.com>
2022-07-14 19:53:45 +0900

    스터브 추가

--- common/modbus/ModbusPointExporter.java
+++ common/modbus/ModbusPointExporter.java
@@ -4,7 +4,23 @@ import java.util.ArrayList;
 
 public class ModbusPointExporter {
 
-	public static void export(ArrayList<ModbusWatchPoint> pointList) {
+	public static void export(int mkVersion, ArrayList<ModbusWatchPoint> pointList) {
+		
+	}
+	
+	public static void exportXML(ArrayList<ModbusWatchPoint> pointList) {
+		
+	}
+	
+	public static void exportExcelV4(ArrayList<ModbusWatchPoint> pointList) {
+		
+	}
+	
+	public static void exportExcelV10_Modbus(ArrayList<ModbusWatchPoint> pointList) {
+		
+	}
+	
+	public static void exportExcelV10_PLC(ArrayList<ModbusWatchPoint> pointList) {
 		
 	}
 	
